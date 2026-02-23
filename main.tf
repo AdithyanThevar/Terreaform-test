592614f89b42cb8ac6e4265d23b56fe271e82187
Adithyan Thevar <87374952+AdithyanThevar@users.noreply.github.com>
2026-02-23 17:17:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "example" {
   ami           = "ami-0f5ee92e2d63afc18" # Amazon Linux 2 (ap-south-1)
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
 
   tags = {
     Name        = "github-actions-ec2"
