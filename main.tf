f937fa9e113676ef1ec857457681d55b994086d6
Adithyan Thevar <87374952+AdithyanThevar@users.noreply.github.com>
2026-02-23 17:16:39 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "example" {
+  ami           = "ami-0f5ee92e2d63afc18" # Amazon Linux 2 (ap-south-1)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name        = "github-actions-ec2"
+    Environment = "dev"
+  }
+}
